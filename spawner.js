044a483210e2e660dfe4ab985ce9299acdaa7960
Lars Bergmann <lars@bergmann82.de>
2024-03-17 18:57:15 +0100

    Only increment cur source counter if we really spawn harvester

--- spawner.js
+++ spawner.js
@@ -11,14 +11,15 @@ var spawner = {
 
         var cur_room = Game.rooms[room];
 
-        //console.log("Cur room energy: " + cur_room.energyAvailable + "/" + cur_room.energyCapacityAvailable);
+        const cur_spawn = cur_room.find(FIND_MY_SPAWNS)[0];
 
         for(var name in Memory.creeps) {
 
             if(!Game.creeps[name]) {
-                // XXX: Make this room aware as soon as all creeps have room in memory
                 if (Memory.creeps[name].role == 'lean_harvester') {
-                    Memory.rooms[Memory.creeps[name].room].sources[Memory.creeps[name].source].cur -= 1;
+                    if (Memory.creeps[name].source) {
+                        Memory.rooms[Memory.creeps[name].room].sources[Memory.creeps[name].source].cur -= 1;
+                    }
                 }
                 delete Memory.creeps[name];
                 console.log('Clearing non-existing creep memory:', name);
@@ -99,33 +100,42 @@ var spawner = {
             }
         }
 
-        if (!Game.spawns['Home'].spawning) {
+        if (!cur_spawn.spawning) {
             for(var role in roles) {
                 var role_settings = roles[role];
                 var role_creeps =  _.filter(Game.creeps, (creep) => creep.memory.role == role);
                 if (role_creeps.length < role_settings.limit) {
                     var newName = role + Game.time;
-                    console.log("Spawning new: " + newName);
     
-                    if (role == 'lean_harvester') {
-                        for(var source_index in cur_room.memory.sources) {
-                            var cur_source = cur_room.memory.sources[source_index];
-                            if (cur_source.cur < cur_source.limit) {
-                                cur_source.cur += 1;
-                                //Memory.rooms[room].sources[source_index].cur += 1;
-                                role_settings.memory['source'] = source_index;
-                                console.log("lean_harvester found source: " + source_index)
-                                break;
-                            }
-                        }
-                    }
-                    Game.spawns['Home'].spawnCreep(
+                    var canspawn = cur_spawn.spawnCreep(
                         role_settings.body,
                         newName,
                         {
                             memory: role_settings.memory,
+                            dryRun: true,
                         }
                     )
+                    if (canspawn == 0) {
+                        console.log("Spawning new: " + newName);
+                        cur_spawn.spawnCreep(
+                            role_settings.body,
+                            newName,
+                            {
+                                memory: role_settings.memory,
+                            }
+                        )
+                        if (role == 'lean_harvester') {
+                            for(var source_index in cur_room.memory.sources) {
+                                var cur_source = cur_room.memory.sources[source_index];
+                                if (cur_source.cur < cur_source.limit) {
+                                    cur_source.cur += 1;
+                                    role_settings.memory['source'] = source_index;
+                                    console.log("lean_harvester found source: " + source_index)
+                                    break;
+                                }
+                            }
+                        }
+                    }
                     return;
                 }
             }
